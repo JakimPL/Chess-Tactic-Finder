63652b34959a818eb4c03f67636a7d9531d8380b
JakimPL <jakimpl@gmail.com>
2023-09-13 19:54:54 +0200

    Added: configuration setter

--- modules/configuration.py
+++ modules/configuration.py
@@ -1,5 +1,19 @@
-from modules.json import json_load
+from modules.json import json_load, json_save
+
+CONFIGURATION_PATH = "configuration.json"
 
 
-def load_configuration(path: str = "configuration.json") -> dict | list:
+def load_configuration(path: str = CONFIGURATION_PATH) -> dict | list:
     return json_load(path)
+
+
+def set_field(key: str, value: any, path: str = CONFIGURATION_PATH):
+    configuration = load_configuration(path)
+
+    configuration_part = configuration
+    key_parts = key.split('.')
+    for key_part in key_parts[:-1]:
+        configuration_part = configuration_part[key_part]
+
+    configuration_part[key_parts[-1]] = value
+    json_save(configuration, path)
