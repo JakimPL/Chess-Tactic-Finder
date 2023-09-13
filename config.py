331a3c65e08056edf0219155df8e3e5868ce7a90
JakimPL <jakimpl@gmail.com>
2023-09-13 19:49:54 +0200

    Added: configuration setter

---
+++ config.py
@@ -0,0 +1,15 @@
+import argparse
+
+from modules.configuration import set_field
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'kv', type=str, nargs=2,
+        help='Key and value, eg. "paths.pgn_extract ./pgn-extract"'
+    )
+
+    args = parser.parse_args()
+    key, value = args.kv
+    set_field(key, value)
+    print(f'Successfully set value {key} to {value}.')
