4be83b7002d961167160bf3acd4dde49399088ec
JakimPL <jakimpl@gmail.com>
2023-10-17 16:30:15 +0200

    Intensified: move colors

--- js/color.js
+++ js/color.js
@@ -9,12 +9,12 @@ class SquareColor {
 var lightSquareColor = '#f0d9b5'
 var darkSquareColor = '#b58863'
 
-var brilliantMoveColor = new SquareColor('#1baca6', '#86c3ae', '#689a85')
-var greatMoveColor = new SquareColor('#5c8bb0', '#a6b2b3', '#898a8a')
-var bestMoveColor = new SquareColor('#95bb4a', '#c3ca80', '#a5a257')
-var inaccuracyColor = new SquareColor('#e8a825', '#ecc16d', '#cf9844')
-var mistakeColor = new SquareColor('#e87a25', '#ecaa6d', '#cf8144')
-var missColor = new SquareColor('#ee6b55', '#efa285', '#d27a5c')
-var blunderColor = new SquareColor('#ca3431', '#dd8773', '#c05e4a')
+var brilliantMoveColor = new SquareColor('#1baca6', '#62BBAB', '#4EA090')
+var greatMoveColor = new SquareColor('#5c8bb0', '#8DA5B2', '#7A8A96')
+var bestMoveColor = new SquareColor('#95bb4a', '#B3C56E', '#A0AA52')
+var inaccuracyColor = new SquareColor('#e8a825', '#EBB855', '#D79D3A')
+var mistakeColor = new SquareColor('#e87a25', '#EB9A55', '#D77F3A')
+var missColor = new SquareColor('#ee6b55', '#EF9075', '#DB755A')
+var blunderColor = new SquareColor('#ca3431', '#D76B5D', '#C35042')
 
 var highlightColor = new SquareColor(darkSquareColor, darkSquareColor, darkSquareColor)
