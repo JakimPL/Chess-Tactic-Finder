35dcf90f9cdfb5ebcf848d86a5f57440360044de
JakimPL <jakimpl@gmail.com>
2025-03-07 14:51:05 +0100

    Fixed: bug with saving configuration

--- modules/requests/configuration.py
+++ modules/requests/configuration.py
@@ -2,4 +2,10 @@ from pydantic import BaseModel
 
 
 class Configuration(BaseModel):
+    algorithm: dict
+    review: dict
+    stockfish: dict
     paths: dict
+    export: dict
+    server: dict
+    tactic_player: dict
